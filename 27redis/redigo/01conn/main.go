9925a58d605156a41409ac5054e73f9bcff19a49
xuwuruoshui <1085252985@qq.com>
2022-03-22 22:16:02 +0800

    rabbitmq简单使用

--- 27redis/redigo/01conn/main.go
+++ 27redis/redigo/01conn/main.go
@@ -20,6 +20,7 @@ func init() {
 		MaxIdle: 16, //最初的连接数量
 		// MaxActive:1000000,    //最大连接数量
 		MaxActive:   0,   //连接池最大连接数量,不确定可以用0（0表示自动定义），按需分配
+		Wait:        true, //超过最大连接数时，是等待还是报错
 		IdleTimeout: 300, //连接关闭时间 300秒 （300秒不使用自动关闭）
 		Dial: func() (redis.Conn, error) { //要连接的redis数据库
 			return redis.Dial("tcp", "192.168.0.110:6379")
---
+++ 34rabbitmq/01normal/main.go
@@ -0,0 +1,111 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"github.com/streadway/amqp"
+	"sync"
+	"time"
+)
+
+/**
+* @creator: xuwuruoshui
+* @date: 2022-03-19 16:51:34
+* @content: rabbitmq简单使用
+ */
+
+var Conn *amqp.Connection
+
+func init() {
+	conn, err := amqp.Dial("amqp://root:root@192.168.0.110:5672/")
+	if err != nil {
+		fmt.Println("连接失败: ", err)
+		return
+	}
+	Conn = conn
+}
+
+// 发布消息
+func Publish(exchange, queueName, body string) (err error) {
+	// 1.创建Channel
+	channel, err := Conn.Channel()
+	if err != nil {
+		fmt.Println("创建channel失败: ", err)
+		return
+	}
+	defer channel.Close()
+
+	// 2.创建队列
+	que, err := channel.QueueDeclare(queueName, false, false, false, false, nil)
+	if err != nil {
+		fmt.Println("创建queue失败: ", err)
+		return
+	}
+
+	// 3.发送消息
+	channel.Publish(exchange, que.Name, false, false, amqp.Publishing{
+		ContentType: "text/plain",
+		Body:        []byte(body),
+	})
+	return
+}
+
+type CallBack func(msg string)
+
+// 接受消息
+func Consumer(exchange, queueName string, ctx context.Context, callback CallBack) (err error) {
+	// 1.创建Channel
+	channel, err := Conn.Channel()
+	if err != nil {
+		fmt.Println("创建channel失败: ", err)
+		return
+	}
+	defer channel.Close()
+
+	// 2.创建队列
+	que, err := channel.QueueDeclare(queueName, false, false, false, false, nil)
+	if err != nil {
+		fmt.Println("创建queue失败: ", err)
+		return
+	}
+
+	msgs, err := channel.Consume(que.Name, "", true, false, false, false, nil)
+	if err != nil {
+		fmt.Println("消费失败: ", err)
+		return
+	}
+	fmt.Println("等待消息来临............")
+	// 读取消息
+	for {
+		select {
+		case d := <-msgs:
+			callback(string(d.Body))
+		case <-ctx.Done():
+			goto End
+		}
+	}
+End:
+	return
+}
+
+var wg sync.WaitGroup
+
+func main() {
+	defer Conn.Close()
+
+	ctx, cancelFuc := context.WithCancel(context.Background())
+	wg.Add(1)
+	go func() {
+		for i := 0; i < 10; i++ {
+			Publish("", "rabbitmq-test", "hello rabbitmq"+string(i))
+			time.Sleep(time.Second)
+		}
+		wg.Done()
+		cancelFuc()
+	}()
+
+	Consumer("", "rabbitmq-test", ctx, func(msg string) {
+		fmt.Println("msg is :", msg)
+	})
+	wg.Wait()
+}

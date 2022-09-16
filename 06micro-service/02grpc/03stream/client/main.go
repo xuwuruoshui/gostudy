c4b94fea84df5039fd5062a76bd6f0b50470cb01
xuwuruoshui <1085252985@qq.com>
2022-09-16 23:05:29 +0800

    stream

---
+++ 06micro-service/02grpc/03stream/client/main.go
@@ -0,0 +1,102 @@
+package main
+
+import (
+	"02grpc/03stream/proto/pb"
+	"context"
+	"fmt"
+	"google.golang.org/grpc"
+	"sync"
+	"time"
+)
+
+func main(){
+	conn, err := grpc.Dial("localhost:9091", grpc.WithInsecure())
+	if err != nil {
+		panic(err)
+	}
+
+	client := pb.NewFoodServiceClient(conn)
+	// 1. 服务端流模式
+	//ServerStream(err, client)
+
+	// 2. 客户端流模式
+	//ClientStream(err, client)
+
+	// 3. 双向模式
+	fullClient, err := client.FullStream(context.Background())
+	if err!=nil{
+		panic(err)
+	}
+
+	wg := FullStream(err, fullClient)
+
+	wg.Wait()
+}
+
+func FullStream(err error, fullClient pb.FoodService_FullStreamClient) sync.WaitGroup {
+	var wg sync.WaitGroup
+	if err != nil {
+		panic(err)
+	}
+	wg.Add(2)
+	go func() {
+		defer wg.Done()
+		for {
+			recv, err := fullClient.Recv()
+			if err != nil {
+				fmt.Println(err)
+				break
+			}
+			fmt.Println(recv.Msg)
+		}
+	}()
+
+	go func() {
+		defer wg.Done()
+		foods := []string{"东坡肘子", "回锅肉", "叫花鸡", "伤心凉粉", "担担面"}
+		for _, item := range foods {
+			err := fullClient.Send(&pb.FoodStreamRequest{Name: item})
+			if err != nil {
+				fmt.Println(err)
+				break
+			}
+		}
+	}()
+	return wg
+}
+
+func ClientStream(err error, client pb.FoodServiceClient) {
+	ctx, cancelFunc := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancelFunc()
+	clientPost, err := client.PostName(ctx)
+	if err != nil {
+		panic(err)
+	}
+
+	foods := []string{"东坡肘子", "回锅肉", "叫花鸡", "伤心凉粉", "担担面"}
+
+	for _, item := range foods {
+		err = clientPost.Send(&pb.FoodStreamRequest{Name: item})
+		time.Sleep(time.Second)
+		if err != nil {
+			fmt.Println(err)
+			break
+		}
+	}
+}
+
+func ServerStream(err error, client pb.FoodServiceClient) {
+	res, err := client.SayName(context.Background(), &pb.FoodStreamRequest{Name: "奥里给"})
+	if err != nil {
+		panic(err)
+	}
+
+	for {
+		recv, err := res.Recv()
+		if err != nil {
+			fmt.Println(err)
+			break
+		}
+		fmt.Println(recv.Msg)
+	}
+}
---
+++ 06micro-service/02grpc/03stream/server/main.go
@@ -0,0 +1,92 @@
+package main
+
+import (
+	"02grpc/03stream/proto/pb"
+	"fmt"
+	"google.golang.org/grpc"
+	"net"
+	"sync"
+)
+
+type FoodInfo struct {
+	pb.UnimplementedFoodServiceServer
+}
+
+// 服务端
+func (f *FoodInfo) SayName(req *pb.FoodStreamRequest,server pb.FoodService_SayNameServer) error {
+	err := server.Send(&pb.FoodStreamResponse{Msg: "您点的菜已经做好了:" + req.Name})
+	if err!=nil{
+		panic(err)
+	}
+	return nil
+}
+
+// 客户端
+func (f *FoodInfo) PostName(server pb.FoodService_PostNameServer) error {
+	for{
+		recv, err := server.Recv()
+		if err!=nil{
+			fmt.Println(err)
+			return err
+		}
+		fmt.Println(recv.Name)
+	}
+}
+
+// 双向通信
+func (f *FoodInfo) FullStream(server pb.FoodService_FullStreamServer) error {
+
+	var wg sync.WaitGroup
+
+	foods := make(chan string,5)
+
+	wg.Add(2)
+
+	// 收
+	go func() {
+		defer wg.Done()
+		for{
+			recv, err := server.Recv()
+			if err!=nil{
+				fmt.Println(err)
+				break
+			}
+			foods<-recv.Name
+			fmt.Println(recv.Name)
+		}
+	}()
+
+	// 发
+	go func() {
+		defer wg.Done()
+		for{
+			select {
+			case food :=<-foods:
+				err := server.Send(&pb.FoodStreamResponse{Msg: "先生你点的菜做好了:"+food})
+				if err!=nil{
+					fmt.Println(err)
+					goto End
+				}
+			}
+		}
+		End:
+	}()
+
+	wg.Wait()
+	return nil
+}
+
+func main() {
+
+	server := grpc.NewServer()
+	pb.RegisterFoodServiceServer(server,&FoodInfo{})
+	listen, err := net.Listen("tcp", ":9091")
+	if err!=nil{
+		panic(err)
+	}
+	err = server.Serve(listen)
+	if err!=nil{
+		panic(err)
+	}
+
+}
